eaf072e31054980be03ea802ce730114756b0eba
Bas <30810@ma-web.nl>
2022-04-29 15:45:51 +0200

    Menu navigation and Player slection is saved

---
+++ Voedelstjes/Assets/Scripts/Menu's/PlayerSlecet.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class PlayerSlecet : MonoBehaviour
+{
+    public void Strawberry()
+    {
+        string json = File.ReadAllText(Application.dataPath + "/Json/PlayerSlecet.json");
+        Chose load = JsonUtility.FromJson<Chose>(json);
+
+        load.strawberry = true;
+        load.kiwi = false;
+
+        string save = JsonUtility.ToJson(load);
+        File.WriteAllText(Application.dataPath + "/Json/PlayerSlecet.json", save);
+        Debug.Log(json);
+        
+        //SceneManager.LoadScene("Level");
+    }
+    public void Kiwi()
+    {
+        string json = File.ReadAllText(Application.dataPath + "/Json/PlayerSlecet.json");
+        Chose load = JsonUtility.FromJson<Chose>(json);
+        
+        load.kiwi = true;
+        load.strawberry = false;
+        
+        string save = JsonUtility.ToJson(load);
+        File.WriteAllText(Application.dataPath + "/Json/PlayerSlecet.json", save);
+        Debug.Log(json);
+        
+        //SceneManager.LoadScene("Level");
+    }
+
+    class Chose
+    {
+        public bool strawberry;
+        public bool kiwi;
+    }
+}

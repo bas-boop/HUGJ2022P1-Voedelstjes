5a4bb85fe0ac4f94cc84d33b3586d4e03f2055fb
Bas <30810@ma-web.nl>
2022-05-04 19:24:03 +0200

    Added story explanation

--- Voedelstjes/Assets/Scripts/Menu's/MainMenu.cs
+++ Voedelstjes/Assets/Scripts/Menu's/MainMenu.cs
@@ -1,13 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject main;
+    [SerializeField] private GameObject story;
+    [SerializeField] private GameObject backButton;
+    [SerializeField] private GameObject storyButton;
+
+    private void Start()
+    {
+        //es = GetComponent<EventSystem>().currentSelectedGameObject;
+    }
+
     public void PlayButton()
     {
         SceneManager.LoadScene("PlayerSlecetScreen");
+    }
+
+    public void StoryButton()
+    {
+        main.SetActive(false);
+        story.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(backButton);
+    }
+
+    public void BackButton()
+    {
+        story.SetActive(false);
+        main.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(storyButton);
     }
     public void QuitButton()
     {
